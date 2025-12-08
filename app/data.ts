638a712a30ef18d0e43cf43275df0564f78309b4
YuvBhatt-Dev <204989587+YuvBhatt-YB@users.noreply.github.com>
2025-12-08 16:38:11 +0530

    minor fixes

--- app/data.ts
+++ app/data.ts
@@ -12,7 +12,7 @@ export const projects = [
         pId:2,
         project_name:"project 2",
         project_img:"/project/project-2.webp",
-        project_main_img:"/project-2/main.jpg",
+        project_main_img:"/project-2/main.webp",
         slider_imgs: ["/project-2/slider/img-1.webp","/project-2/slider/img-2.webp","/project-2/slider/img-3.webp","/project-2/slider/img-4.webp","/project-2/slider/img-5.webp"],
         stack:["Next.js","Prisma","MongoDB","Redis","Typescript"],
         role_description:"I developed an open-source link-in-bio platform that helps creators and professionals combine all their social links into a single customizable landing page. My main contributions were building the frontend UI, creating a flexible card-based layout system, and integrating a solid backend for link management. The platform delivers a clean, modern interface with smooth UX, making it simple for users to personalize and share their social presence."
@@ -21,7 +21,7 @@ export const projects = [
         pId:3,
         project_name:"project 3",
         project_img:"/project/project-3.webp",
-        project_main_img:"/project-3/main.jpg",
+        project_main_img:"/project-3/main.webp",
         slider_imgs: ["/project-3/slider/img-1.webp","/project-3/slider/img-2.webp","/project-3/slider/img-3.webp","/project-3/slider/img-4.webp","/project-3/slider/img-5.webp"],
         stack:["HTML","CSS","Javascript","Figma"],
         role_description:"I built an automated job-scraping and alerting tool that fetches real-time job postings from multiple sites using BeautifulSoup and Selenium. My role involved writing efficient scraping logic, data extraction pipelines, and a Streamlit dashboard for viewing results. The system stores job titles and URLs in CSV format and instantly notifies users of new openings, making job searching faster and more actionable."
@@ -30,7 +30,7 @@ export const projects = [
         pId:4,
         project_name:"project 4",
         project_img:"/project/project-4.webp",
-        project_main_img:"/project-4/main.jpg",
+        project_main_img:"/project-4/main.webp",
         slider_imgs: ["/project-4/slider/img-1.webp","/project-4/slider/img-2.webp","/project-4/slider/img-3.webp","/project-4/slider/img-4.webp","/project-4/slider/img-5.webp"],
         stack:["React","Node.js","Express.js","Prisma","PostgreSQL","Redis","Typescript"],
         role_description:"I created an AI-powered pair-programmer designed to assist developers with coding tasks across Python, SQL, and JavaScript. My work focused on building the Next.js frontend, designing the chat UI, and integrating intelligent code-generation workflows. The tool provides real-time coding suggestions, error explanations, and project-aware assistance—making it a practical companion for developers working on complex projects."
