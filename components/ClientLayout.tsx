c9134cf63cbe1088b7934b21959caa17ca727313
YuvBhatt-Dev <204989587+YuvBhatt-YB@users.noreply.github.com>
2025-12-09 13:07:24 +0530

    Lenis UseEffect Updated

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -3,24 +3,36 @@
 
 import Navbar from "./navbar"
 
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import Lenis from "lenis"
 import Overlay from "./Overlay"
 import NavOverlay from "./NavOverlay"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
 
 
-
+gsap.registerPlugin(ScrollTrigger)
 export default function ClientLayout({children}:Readonly<{children: React.ReactNode}>){
+    const lenisRef = useRef<Lenis | null>(null)
+    const tickerCallback = useRef<((time:number) => void)| null>(null)
     
     useEffect(()=>{
         const lenis = new Lenis()
-        function raf(time: any){
-            lenis.raf(time)
-            requestAnimationFrame(raf)
+        lenisRef.current = lenis
+        lenis.on("scroll",ScrollTrigger.update)
+        
+        tickerCallback.current = (time:number) => {
+            lenis.raf(time*1000)
         }
-        requestAnimationFrame(raf)
+        gsap.ticker.add(tickerCallback.current)
+
+        gsap.ticker.lagSmoothing(0)
+
+        return () => {
+            if(tickerCallback.current) gsap.ticker.remove(tickerCallback.current)
+            lenis.destroy()
+        }
     },[])
-    
     return (
         <>
         
